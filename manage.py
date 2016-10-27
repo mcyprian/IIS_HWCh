8808461751ede7c40d03f4c53b55f2ec52e9da03
Michal Cyprian <mcyprian@redhat.com>
2016-10-27 18:03:37 +0200

    Add manage command to recreate whole database

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager, Shell
 
 from app import create_app, db
 from app.storage import *
-from db_init import fill_db
+from db_init import fill_db, recreate_all
 
 app = create_app(os.environ.get('IIS_CONFIG', 'default'))
 manager = Manager(app)
@@ -17,6 +17,17 @@ def make_shell_context():
                 PlayedIn=PlayedIn, fill_db=fill_db)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
+@manager.command
+def recreate_db():
+    print("Dropping and recreating all tables...")
+    recreate_all()
+    print("Inserting data to recreated tables...")
+    fill_db()
+    db.session.commit()
+    print("Done.")
+
 
 if __name__ == '__main__':
     manager.run()
