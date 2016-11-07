a21f41577428c36de6b55c7a11520e1288261b12
Marian Durco <majodurco@gmail.com>
2016-11-07 22:59:07 +0100

    removed console logs from js scripts

--- app/static/js/teams.js
+++ app/static/js/teams.js
@@ -2,9 +2,6 @@ import getData from "./data"
 import { keys,values } from "underscore"
 
 export function createTeamList(data){
-	console.log("creating TEAM LIST");
-	window.data = data;
-	console.log(data);
 	let team;
 	for(let i=0;i<data.length;i++) {
 		team = `<a href="#/" class="list-group-item team-in-list" id="${i}">${data[i]}</a><div id="teamId${i}"><p>Team Data</p></div>`;
@@ -14,10 +11,8 @@ export function createTeamList(data){
 }	
 
 function toggleTeamInfo(){
-	console.log("TOGGLE!!!!");
 	$(".team-in-list").click(function(){
 		var id = $(this).attr("id");
-		console.log("clicked on " + id);
 		$("#teamId"+id).slideToggle("slow");
 	});
 };
