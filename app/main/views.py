76ac150243b7f2003e5687c99b4ba65cd30613f8
Marian Durco <majodurco@gmail.com>
2016-11-20 14:40:03 +0100

    Complete filter in standings

--- app/main/views.py
+++ app/main/views.py
@@ -100,7 +100,7 @@ def player_profile(player_surname, user=None):
 @main.route("/standings")
 @check_current_user
 def standings(user=None):
-    return render_template('blank.html', data="Various standings...", user=user)
+    return render_template('standings.html', data="Various standings...", user=user)
 
 
 @main.route("/standings/data.json")
@@ -121,11 +121,6 @@ def standings_data():
         }
         players.append(player_data)
     return jsonify(players)
-
-def standings():
-    return render_template('standings.html', data="Various standings...")
->>>>>>> Create base for standings
-
 
 @main.route("/secret")
 @check_current_user
--- app/static/js/standings.js
+++ app/static/js/standings.js
@@ -1,9 +1,33 @@
+import { sortBy } from "underscore"
 import getData from "./data"
 
-var standings_data
+var STANDINGS_DATA
+var POSITION_TOGGLE = false;
 
 export function standings() {
-	console.log("standings");
+	showTable();
+	$("#filter-select1").change( () => {
+		POSITION_TOGGLE = false;
+		showTable()
+	});
+	$('#range input').change( () => {
+		POSITION_TOGGLE = false;
+		showTable()
+	});
+}
+
+function showTable() {
+	if( $("#filter-select1").val() !== "ChooseFilter")
+	{
+		if (STANDINGS_DATA === undefined) {
+			getData("/data.json", data => {
+				createStandingTable(data) 
+			});
+		}
+		else {
+			createStandingTable(STANDINGS_DATA);
+		}
+	}
 }
 
 function but() {
@@ -14,7 +38,7 @@ function but() {
 
 function forwardData() {
 	console.log("getting data");
-	getData("/list.json",filterData);
+	getData("/list.json", filterData);
 }
 
 function filterData(data) {
@@ -29,9 +53,16 @@ function filterData(data) {
 }
 
 function createStandingTable(data) {
-	let standings_data = data
-	console.log(`standings_DATA: ${standings_data}`)
+	STANDINGS_DATA = data
+	window.data = STANDINGS_DATA;
+	const filterString = $("#filter-select1").val().toLowerCase()
+	STANDINGS_DATA = sortBy(STANDINGS_DATA, 'name').reverse()
+	STANDINGS_DATA = sortBy(STANDINGS_DATA, filterString).reverse()
 	$("#table-wrap").html(finalTable())
+	$("#pos-toggle").click( () => {
+		POSITION_TOGGLE = !POSITION_TOGGLE;
+		showTable()
+	});
 }
 
 function fillAdditionalOpt() {
@@ -39,24 +70,54 @@ function fillAdditionalOpt() {
 	return `<th>${option}</th>`;
 }
 
-function fillTableData() {
-	return ""
+function fillTableData(range) {
+	let result = ""
+	if (range=="ALL")
+		range = STANDINGS_DATA.length
+	if (range > STANDINGS_DATA.length)
+		range = STANDINGS_DATA.length
+	if(!POSITION_TOGGLE) {
+		for(let i=0; i<range; i++) {
+			result += wrapPlayerData(i+1, STANDINGS_DATA[i])
+		}
+	}
+	else {
+		for(let i=range-1; i>=0; i--) {
+			result += wrapPlayerData(i+1, STANDINGS_DATA[i])
+		}
+	}
+	return result
+}
+
+function wrapPlayerData(pos, player_data) {
+	const filter_select =  $("#filter-select1").val().toLowerCase()
+	return `<tr>
+	<td>${pos}</td>
+	<td>${player_data.name}</td>
+	<td>${player_data.team}</td>
+	<td>${player_data.position}</td>
+	<td>${player_data[filter_select]}</td>
+	<tr>`
+}
+
+function getRange() {
+	let	range_value = $('input[name=range]:checked', '#range').val(); 
+	return range_value;
 }
 
 function finalTable() {
 	let additional_option = fillAdditionalOpt();
-	let table_data = fillTableData();
+	let table_data = fillTableData(getRange());
+	let position_icon = "glyphicon-sort-by-order";
+	if(POSITION_TOGGLE)
+		position_icon = "glyphicon-sort-by-order-alt";
 	let table_core = 
 	`<table class="table">
 		<thead>
 			<tr>
-				<th><span class="glyphicon glyphicon-sort-by-order"></span> Pos</th>
+				<th><button id="pos-toggle"><span class="glyphicon ${position_icon}"></span> Pos</button></th>
 				<th>Name</th>
-				<th> 
-					<select>
-						<option value="">SVK</option>
-					</select>
-				</th>
+				<th>Team</th>
 				<th>Position</th>
 				${additional_option}
 			</tr>
@@ -69,22 +130,3 @@ function finalTable() {
 	</table>`;
 	return table_core;
 }
-
-// self executable
-$(function() {
-	$("#filter-select1").change(function() {
-		if( $("#filter-select1").val() !== "ChooseFilter")
-		{
-			if (standings_data === undefined) {
-				getData("/list.json", data => { // TODO repl URL
-					createStandingTable(data) 
-				});
-				console.log("downloading data");
-			}
-			else {
-				console.log("data are here dont have to donwload them");
-				createStandingTable(standings_data);
-			}
-		}
-	});
-});
