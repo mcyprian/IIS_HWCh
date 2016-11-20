4cd90e879b9ccaab133617e2bb8b2b6e1ec77a87
Michal Cyprian <mcyprian@redhat.com>
2016-11-20 17:52:29 +0100

    Add check_user to errors, fixes #2

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,17 +1,23 @@
 from flask import render_template
 
 from app.main import main
+from app.roles import check_current_user
 
 
 @main.app_errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@check_current_user
+def page_not_found(e, user=None):
+    print(user)
+    return render_template('404.html', user=user), 404
 
 
 @main.app_errorhandler(500)
-def internal_error(e):
-    return render_template('500.html'), 500
+@check_current_user
+def internal_error(e, user=None):
+    return render_template('500.html', user=user), 500
+
 
 @main.app_errorhandler(403)
-def forbidden(e):
-    return render_template('403.html'), 403
+@check_current_user
+def forbidden(e, user=None):
+    return render_template('403.html', user=user), 403
