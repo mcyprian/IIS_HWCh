9e2491351aeca1e566e7ad7b42ac4ddeb8d7b6e6
Marian Durco <majodurco@gmail.com>
2016-11-20 21:48:32 +0100

    Add team data and link

--- app/static/js/teams.js
+++ app/static/js/teams.js
@@ -2,51 +2,131 @@ import getData from "./data"
 import { keys } from "underscore"
 
 export function teams(){
-	getData("/list.json",createTeamList);
+	getData("/list.json", createTeamList);
 }
 
 function createTeamList(data){
-	let team;
-	for(let i=0;i<data.length;i++) {
-		let flagUrl = getFlag(data[i]);
-		team = `<div class="panel panel-primary">
+	const teams = keys(data);
+	let html_result;
+	let flagUrl;
+	let team_body =  "";
+	window.data = data;
+
+	for(let i = 0; i < teams.length; i++) {
+		let country_name = teams[i];
+		flagUrl = getCountryFlag(getCountryCode(country_name));
+		team_body = createTeamBody(data[country_name], country_name);
+
+		html_result = `<div class="panel panel-primary">
 		<div class="panel-heading team-in-list" id="${i}">
 				<img class="team-logo-heading" src="${flagUrl}">
-				${data[i]}
+				${country_name}
 		</div>
-		<div class="panel-body"
+		<div class="panel-body">
 			<div id="teamId${i}">
-				<p>Team Data</p>
+				${team_body}
 			</div>
 		</div>
 		</div>`;
-		$(".list-group").append(team);
+		$(".list-group").append(html_result);
 	}
 	toggleTeamInfo();
 }	
 
+function createTeamBody(country_data, country_name) {
+	console.log("IN team body ",country_data)
+	const county_header = getCountryHeader(country_name);
+	const mvp_player = createPersonDiv(country_data, "mvp");
+	const coach = createPersonDiv(country_data, "coach");
+	return `
+	<div class="team-header-link">
+		<h3>${county_header} National Hockey Team</h3>
+		<a href="/teams/${country_name}" class="btn btn-info" role="button">Team Details</a>
+	</div>
+	<hr>
+	<div class="row">
+		${mvp_player}
+		${coach}
+	</div>`;
+}
+
+function createPersonDiv (country_data, position) {
+	console.log("IN person div ",country_data)
+	const mvp_or_coach = (position === "mvp" ? "The Most Valuable Player": "Coach");
+	const name = (position === "mvp" ? country_data.mvp.full_name : country_data.coach.full_name);
+	let link = "";
+	let points = "";
+
+	if (position === "mvp")
+	{
+		link = `<a href="players/${country_data.mvp.id}" class="btn btn-primary" role="button">View profile</a>`;
+		points = `<p>Points:${country_data.mvp.points}</p>`;
+	}
+	return `
+	<div class="col-md-6">
+		<h3>${mvp_or_coach}</h3>
+		<div class="thumbnail">
+			<div class="caption">
+				<div class="row">
+					<div class="col-md-6">
+						<h3>${name}</h3>
+						${points}
+						${link}
+					</div>
+					<div class="col-md-6">
+						<img src="https://mcyprian.fedorapeople.org/avatar.png" class="avatar-img">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>`;
+}
+
 function toggleTeamInfo(){
 	$(".team-in-list").click(function(){
 		var id = $(this).attr("id");
-		$("#teamId"+id).slideToggle("slow");
+		$("#teamId" + id).slideToggle("slow");
 	});
 }
 
-function getFlag(country) {
+function getCountryHeader(country) {
 	const flags = {
-		Canada: "https://mcyprian.fedorapeople.org/flags/CAN",
-		Slovakia: "https://mcyprian.fedorapeople.org/flags/SVK",
-		Finland: "https://mcyprian.fedorapeople.org/flags/FIN",
-		USA: "https://mcyprian.fedorapeople.org/flags/USA",
-		Germany: "https://mcyprian.fedorapeople.org/flags/GER",
-		Czech: "https://mcyprian.fedorapeople.org/flags/CZE",
-		Russia: "https://mcyprian.fedorapeople.org/flags/RUS",
-		Sweden: "https://mcyprian.fedorapeople.org/flags/SWE"
+		Canada: "Canada",
+		Slovakia: "Slovak Republic",
+		Finland: "Republic of Finland",
+		USA: "United States of America",
+		Germany: "Federal Republic of Germany",
+		Czech: "Czech Republic",
+		Russia: "Russia Federation",
+		Sweden: "Kingdom Of Sweden"
+	};
+
+	const key_flags = keys(flags);
+
+	for(let i = 0; i < key_flags.length; i++){
+		if(country.indexOf(key_flags[i]) !== -1)
+			return flags[key_flags[i]];
+	}
+}
+function getCountryCode(country) {
+	const flags = {
+		Canada: "CAN",
+		Slovakia: "SVK",
+		Finland: "FIN",
+		USA: "USA",
+		Germany: "GER",
+		Czech: "CZE",
+		Russia: "RUS",
+		Sweden: "SWE"
 	};
 	const key_flags = keys(flags);
 
-	for(let i=0;i<key_flags.length;i++){
-		if(country.indexOf(key_flags[i])!== -1)
+	for(let i = 0; i < key_flags.length; i++){
+		if(country.indexOf(key_flags[i]) !== -1)
 			return flags[key_flags[i]];
 	}
+}
+
+function getCountryFlag(country_code) {
+	return "https://mcyprian.fedorapeople.org/flags/" + country_code
 }
