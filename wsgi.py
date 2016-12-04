f3cac3dd7b4205ac9265f05b99d79e90bb41dbc0
Michal Cyprian <mcyprian@redhat.com>
2016-12-04 22:14:03 +0100

    Add session expiration to wsgi script

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import os
+from datetime import timedelta
+
+from flask import session
 
 from app import create_app
 
 application = create_app(os.environ.get('IIS_CONFIG', 'default'))
+
+
+@application.before_request
+def session_expiration():
+    session.permanent = True
+    application.permanent_session_lifetime = timedelta(minutes=2)
